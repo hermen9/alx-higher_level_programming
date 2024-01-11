2e4d6c8dd85dbccfaa3fe7172e20965dc7ffbef7
hermen9 <donaldkoronya2@gmail.com>
2024-01-11 20:38:14 +0200

    func returns a new dict with vals * 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {key: val*2 for key, val in my_dict.items()}
+    new_dict = {}
+    for i in a_dictionary:
+        new_dict[i] = a_dictionary[i] * 2
+    return new_dict
